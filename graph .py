3f049802aa767e6393e68086c75d184635c91971
wyatt <wyatt.osterling@hotmail.com>
2020-02-09 06:20:54 -0500

    removed table generator by integrating it into graph.py. manual working

--- graph .py
+++ graph .py
@@ -3,27 +3,78 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from math import *
-verticies = (
-    (1, -1, -0),
-    (1,1, 0),
-    (1, 1, -0),
-    (-1,1, 0),
-    (1, 1, -0),
-    (1,1, 1)
-)
-
-edges = (
-    (0,1),
-    (2,3),
-    (4,5)
-)
-
+from numpy import *
+points = []
 def Cube():
+        #else:
+        #    z_cord.append (y.imag)
+    verticies = [
+        (0, -1, -0),
+        (0,1, 0),
+        (1, 0, -0),
+        (-1,0, 0),
+        (0, 0, -1),
+        (0,0, 1)
+    ]
+    edges = [
+        (0,1),
+        (2,3),
+        (4,5)
+    ]
+    graph = [
+        (0,1),
+        (1,2),
+        (2,3),
+
+
+    ]
+
+
+    x= 0
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
             glVertex3fv(verticies[vertex])
+    try:
+        x_cord = (
+            (0),
+            (1),
+            (2),
+            (3),
+            (4),
+            (5)
+        )
 
+        def formula(a):
+            transformations = "(-2)**x"
+            varplace = transformations.find("x")
+            return transformations.replace("x", str(a))
+        for number in x_cord:
+            expression = formula(number)
+            print (expression)
+            y = eval(expression)
+            if (y.imag) == 0:
+                print (y)
+                point = (number, y, 0)
+                print (point)
+                points.append(point)
+                print (points)
+            else:
+                z = y.imag
+                print (z)
+                point = (number, 0, z)
+                print (point)
+                points.append(point)
+                print (points)
+            try:
+                for edge in graph:
+                    for vertex in edge:
+                        glVertex3fv(points[vertex])
+            except:
+                print("graphing failed")
+
+    except:
+        print("calculation failded")
     glEnd()
 def main():
     pygame.init()
@@ -47,14 +98,22 @@ def main():
                     glTranslatef(0.0,1.0,0)
                 if event.key == pygame.K_DOWN:
                     glTranslatef(0.0,-1.0,0)
+                if event.key == pygame.K_w:
+                    print ("you pressed w")
+                    glRotatef(1, 10, 0, 0)
+                if event.key == pygame.K_s:
+                    glRotatef(1, -10, 0, 0)
+                if event.key == pygame.K_a:
+                    glRotatef(1, 10, 0, 0)
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 4:
                     glTranslatef(0,0,1.0)
                 if event.button == 5:
                     glTranslatef(0,0,-1.0)
-        #glRotatef(1, 3, 3, 1)
+        glRotatef(100, 0, 0, 0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
-        pygame.time.wait(10)
+        pygame.time.wait(100)
 main()
