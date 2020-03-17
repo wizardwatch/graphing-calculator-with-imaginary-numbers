92642a2bbdd6c14061fa4f067b43001f2436e069
wyatt <wyatt.osterling@hotmail.com>
2020-03-17 18:48:07 -0400

    finished evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -6,54 +6,101 @@ subtraction = "-"
 def evaluate(equation):
 
     filter = -1
-    operator = []
+    operators = []
     if exponet in equation:
         first_find = 0
         for letter in equation:
-            operator.append (equation.find("^", first_find, first_find + 1))
+            operators.append (equation.find("^", first_find, first_find + 1))
             first_find += 1
-        for number in operator:
-            operator.remove(filter)
-        operator.remove(filter)
+        for number in operators:
+            try:
+                operators.remove(filter)
+            except:
+                pass
+        try:
+            operators.remove(filter)
+        except:
+            pass
         #replace with number
-    print(operator)
-    operator.clear()
+        for  operator in operators:
+            fnum = equation[operator-1]
+            snum = equation[operator+1]
+            answer = float(fnum)**float(snum)
+    operators.clear()
     if multiplication in equation:
         first_find = 0
         for letter in equation:
-            operator.append (equation.find("*", first_find, first_find + 1))
+            operators.append (equation.find("*", first_find, first_find + 1))
             first_find += 1
-        for number in operator:
-            operator.remove(filter)
-        operator.remove(filter)
+        try:
+            operators.remove(filter)
+        except:
+            pass
+        try:
+            operators.remove(filter)
+        except:
+            pass
+        for  operator in operators:
+            fnum = equation[operator-1]
+            snum = equation[operator+1]
+            answer = float(fnum)*float(snum)
     if division in equation:
         first_find = 0
         for letter in equation:
-            operator.append (equation.find("/", first_find, first_find + 1))
+            operators.append (equation.find("/", first_find, first_find + 1))
             first_find += 1
-        for number in operator:
-            operator.remove(filter)
-        operator.remove(filter)
+        for number in operators:
+            try:
+                operators.remove(filter)
+            except:
+                pass
+        try:
+            operators.remove(filter)
+        except:
+            pass
+        for operator in operators:
+            fnum = equation[operator-1]
+            snum = equation[operator+1]
+            answer = float(fnum)/float(snum)
         #replace with number
-    print(operator)
-    operator.clear()
+    print(operators)
+    operators.clear()
     if addition in equation:
         first_find = 0
         for letter in equation:
-            operator.append (equation.find("+", first_find, first_find + 1))
+            operators.append (equation.find("+", first_find, first_find + 1))
             first_find += 1
-        for number in operator:
-            operator.remove(filter)
-        operator.remove(filter)
+        try:
+            operators.remove(filter)
+        except:
+            pass
+    try:
+        operators.remove(filter)
+    except:
+        pass
+    for  operator in operators:
+        fnum = equation[operator-1]
+        snum = equation[operator+1]
+        answer = float(fnum)+float(snum)
     if subtraction in equation:
         first_find = 0
         for letter in equation:
-            operator.append (equation.find("-", first_find, first_find + 1))
+            operators.append (equation.find("-", first_find, first_find + 1))
             first_find += 1
-        for number in operator:
-            operator.remove(filter)
-        operator.remove(filter)
-    print(operator)
-    operator.clear()
-example = "3*3-3"
+        for number in example:
+            try:
+                operators.remove(filter)
+            except:
+                pass
+        try:
+            operators.remove(filter)
+        except:
+            pass
+        for  operator in operators:
+            fnum = equation[operator-1]
+            snum = equation[operator+1]
+            answer = float(fnum)-float(snum)
+        operators.clear()
+    print (answer)
+example = "3-9"
 evaluate(example)
